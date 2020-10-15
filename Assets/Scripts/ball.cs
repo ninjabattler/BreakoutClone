e371f86dcf6b784614dc884ef14faf80295d0b0b
Jayden Tucker <jaydenrtucker@gmail.com>
2020-10-14 18:32:30 -0700

    Added a ball prefab that bounces off the paddle and players can now hold shift to move the paddle faster

---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(1,1);
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Translate(speed.x/100, speed.y/100, 0);
+    }
+
+    void OnCollisionEnter(Collision collision){
+        Debug.Log(collision.gameObject);
+        if(collision.gameObject.name == "Paddle"){
+            speed.y *= -1;
+        }
+    }
+}
